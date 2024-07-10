f07d25e952b68ceb9b7f816a16142b17394e2766
YosTgi <yoseph.tibebu@gmail.com>
2024-07-10 13:07:03 +0300

    RSRV-0002 Add list of  booking time slots with half hour interval

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -13,7 +13,11 @@ const MakeReservation = () => {
     const [roomNumber, setRoomNumber] = useState("");
     const [successMessage, setSuccessMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
-    const [availableSlots, setAvailableSlots] = useState([]);
+
+    const availableSlots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+        "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00",
+        "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
+        "21:00"];
 
     const handleSubmit = async (e) => {
         e.preventDefault();
