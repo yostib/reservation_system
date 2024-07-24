220f1acbdf2d66a5ed526d68338dfa3dda1cd63a
YosTgi <yoseph.tibebu@gmail.com>
2024-07-24 15:09:52 +0300

    RSRV-0018-Add a bare bone calendar with the slots.
    Next implement slot event handler. There is a console error coming
    from react. Investigate the error and maybe fix it.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,59 @@
 import * as PropTypes from "prop-types";
 
-const Calendar = (props) => {
-    return (
-        <div>The calendar</div>
-    );
+const weekDays = ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat", "Sun"]
+const hoursOfTheDays = ["09:00", "10:00", "11:00", "12:00",
+    "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00",
+    "20:00", "21:00"]
+
+const generateHeaders = (day) => {
+    return (<th key={day}>{day}</th>)
 }
+
+const generateSlots = (hour) => {
+    return weekDays.map((day) => {
+        return (
+            <td>
+                <button key={`${day}-${hour}`} value={`${day}-${hour}`} onClick={setReserved}>Available</button>
+            </td>
+        )
+    })
+}
+
+const setReserved = (e) => {
+    console.log(e.target.value)
+}
+
+const generateBodyRows = (hour) => {
+    return (<tr>
+        <td key={hour}>{hour}</td>
+        {generateSlots(hour)}
+    </tr>)
+}
+
+const Calendar = (props) => {
+    return (<div>
+        <div>The calendar</div>
+        <div id="calendar-table-container">
+            <table>
+                <thead>
+                <tr>
+                    <th></th>
+                    {weekDays.map(generateHeaders)}
+                </tr>
+                </thead>
+                <tbody>
+                {hoursOfTheDays.map(generateBodyRows)}
+                </tbody>
+            </table>
+        </div>
+    </div>);
+}
+
+// practice
+// a function called subtraction
+// takes two arguments that are numbers
+// call it/ use it
+
 
 Calendar.propTypes = {children: PropTypes.node};
 
